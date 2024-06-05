49437a8fc56cde88a253e76e08d79259049b27ab
furqhanbinmoosaptr <furqhanbinmoosapr@gmail.com>
2024-06-05 17:26:26 +0530

    modules added

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,11 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'quranfoundationfrontend';
+
+module.exports = {
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? `/${repoName}` : '',
+};
